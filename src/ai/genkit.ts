7bc1785a4c84de63b87de2a68925a9a318c3a6d5
Amit Dafda <dafdaamit206@gmail.com>
2025-08-07 16:15:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,17 +2,10 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 import { Plugin } from 'genkit';
 
-// Sad hack to get around the fact that the NextJS plugin is not yet available.
-const nextPlugin: Plugin<any> = {
-  name: 'next',
-  configure: async (options: any) => {},
-};
-
 
 export const ai = genkit({
   plugins: [
     googleAI(),
-    nextPlugin,
   ],
   model: 'googleai/gemini-2.0-flash',
 });
